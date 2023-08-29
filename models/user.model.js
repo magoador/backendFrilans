b19fc77369efd7abb6e4c04e6e0da81794ea02b8
magoador <malikmagomedov95@mail.ru>
2023-08-29 11:20:59 +0300

    Добавил в модель юзера баланс

--- models/user.model.js
+++ models/user.model.js
@@ -1,49 +1,56 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const userSchema = mongoose.Schema({
-    // Поле для хранения логина пользователя (тип: строка)
-    login: String,
+  // Поле для хранения логина пользователя (тип: строка)
+  login: String,
 
-    // Поле для хранения хешированного пароля пользователя (тип: строка)
-    password: String,
+  // Поле для хранения хешированного пароля пользователя (тип: строка)
+  password: String,
 
-    // Поле для хранения статуса администратора (тип: булево, значение по умолчанию: false)
-    admin: {
-        type: Boolean,
-        default: false,
+  // Поле для хранения статуса администратора (тип: булево, значение по умолчанию: false)
+  admin: {
+    type: Boolean,
+    default: false,
+  },
+
+  // Поле для хранения баланса пользователя
+  balance: Number,
+
+  // Поле для хранения отзывов пользователя
+  reviews: [
+    {
+      user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User", // Ссылка на модель User
+        required: true,
+      },
+      text: {
+        type: String,
+        required: true,
+      },
+      timestamp: {
+        type: Date,
+        default: Date.now,
+      },
     },
+  ],
 
-    // Поле для хранения отзывов пользователя
-    reviews: [{
-        user: {
-            type: mongoose.Schema.Types.ObjectId,
-            ref: 'User', // Ссылка на модель User
-            required: true
-        },
-        text: {
-            type: String,
-            required: true
-        },
-        timestamp: {
-            type: Date,
-            default: Date.now
-        }
-    }],
-
-    // Поле для хранения оценок пользователя
-    score: [{
-        grade: {
-            type: Number,
-            required: true
-        },
-        user: {
-            type: mongoose.Schema.Types.ObjectId,
-            ref: 'User', // Ссылка на модель User
-            required: true
-        },
-    }],
+  // Поле для хранения оценок пользователя
+  score: [
+    {
+      grade: {
+        type: Number,
+        required: true,
+      },
+      user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User", // Ссылка на модель User
+        required: true,
+      },
+    },
+  ],
 });
 
-const User = mongoose.model('User', userSchema);
+const User = mongoose.model("User", userSchema);
 
 module.exports = User;

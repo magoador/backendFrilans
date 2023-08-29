0049663158d6335dc5ef528f3455ae521d755320
magoador <malikmagomedov95@mail.ru>
2023-08-29 12:21:36 +0300

    Подправил роуты

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ const { Router } = require("express");
 const router = Router();
 
 router.use("/users", require("./users.route"))
-router.use("./work", require("./work.route"))
-router.use('./workType', require("./workType.route"))
+router.use("/work", require("./work.route"))
+router.use('/workType', require("./workType.route"))
 
 module.exports = router;
